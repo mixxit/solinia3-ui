8b07a87a744fa8f902c2293712ef85b289a84117
Mike Bolland <michaelbollanduk@gmail.com>
2019-05-11 22:36:38 +0100

    keybinds

--- src/main/java/com/solinia/solinia3ui/solinia3ui.java
+++ src/main/java/com/solinia/solinia3ui/solinia3ui.java
@@ -3,13 +3,12 @@ package com.solinia.solinia3ui;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
+import net.minecraftforge.client.event.InputEvent.KeyInputEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
@@ -19,13 +18,12 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("solinia3ui")
 public class solinia3ui {
 	// Directly reference a log4j logger.
 	private static final Logger LOGGER = LogManager.getLogger();
+	private KeyBinds keyBinds = new KeyBinds();
 
 	public solinia3ui() {
 		// Register the setup method for modloading
@@ -45,12 +43,7 @@ public class solinia3ui {
 		// some preinit code
 		LOGGER.info("HELLO FROM PREINIT");
 		LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-		registerKeyBinds();
-	}
-
-	private void registerKeyBinds() {
-		// TODO Auto-generated method stub
-		KeyBinds.register();
+		keyBinds.registerKeyBinds();
 	}
 
 	private void doClientStuff(final FMLClientSetupEvent event) {
@@ -60,16 +53,14 @@ public class solinia3ui {
 
 	private void enqueueIMC(final InterModEnqueueEvent event) {
 		// some example code to dispatch IMC to another mod
-		InterModComms.sendTo("examplemod", "helloworld", () -> {
-			LOGGER.info("Hello world from the MDK");
-			return "Hello world";
-		});
+		//InterModComms.sendTo("examplemod", "helloworld", () -> {LOGGER.info("Hello world from the MDK");
+		//	return "Hello world";
+		//});
 	}
 
 	private void processIMC(final InterModProcessEvent event) {
 		// some example code to receive and process InterModComms from other mods
-		LOGGER.info("Got IMC {}",
-				event.getIMCStream().map(m -> m.getMessageSupplier().get()).collect(Collectors.toList()));
+		//LOGGER.info("Got IMC {}",event.getIMCStream().map(m -> m.getMessageSupplier().get()).collect(Collectors.toList()));
 	}
 
 	// You can use SubscribeEvent and let the Event Bus discover methods to call
@@ -92,91 +83,99 @@ public class solinia3ui {
 	}
 
 	@SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
-	public static void onKeyEvent(RenderTickEvent event) {
+	public void onKeyEvent(KeyInputEvent event) {
+		if (event.isCanceled())
+			return;
+		
+		if (event.getAction() != 1)
+			return;
+		
 		checkKeys();
 	}
 
-	private static void checkKeys() {
-		if (KeyBinds.targetnearestnpc.isPressed())
+	private void checkKeys() {
+		if (keyBinds.targetnearestnpc.isPressed())
 			targetNearestNpc();
-		if (KeyBinds.toggleautoattack.isPressed())
+		if (keyBinds.toggleautoattack.isPressed())
 			toggleAutoAttack();
-		if (KeyBinds.canceltarget.isPressed())
+		if (keyBinds.canceltarget.isPressed())
 			cancelTarget();
-		if (KeyBinds.targetself.isPressed())
+		if (keyBinds.targetself.isPressed())
 			targetSelf();
-		if (KeyBinds.togglesitstand.isPressed())
+		if (keyBinds.togglesitstand.isPressed())
 			toggleSitStand();
-		if (KeyBinds.targetteammember1.isPressed())
+		if (keyBinds.targetteammember1.isPressed())
 			targetTeamMember(1);
-		if (KeyBinds.targetteammember2.isPressed())
+		if (keyBinds.targetteammember2.isPressed())
 			targetTeamMember(2);
-		if (KeyBinds.targetteammember3.isPressed())
+		if (keyBinds.targetteammember3.isPressed())
 			targetTeamMember(3);
-		if (KeyBinds.targetteammember4.isPressed())
+		if (keyBinds.targetteammember4.isPressed())
 			targetTeamMember(4);
-		if (KeyBinds.targetteammember5.isPressed())
+		if (keyBinds.targetteammember5.isPressed())
 			targetTeamMember(5);
-		if (KeyBinds.castspell1.isPressed())
+		if (keyBinds.castspell1.isPressed())
 			castSpell(1);
-		if (KeyBinds.castspell2.isPressed())
+		if (keyBinds.castspell2.isPressed())
 			castSpell(2);
-		if (KeyBinds.castspell3.isPressed())
+		if (keyBinds.castspell3.isPressed())
 			castSpell(3);
-		if (KeyBinds.castspell4.isPressed())
+		if (keyBinds.castspell4.isPressed())
 			castSpell(4);
-		if (KeyBinds.castspell5.isPressed())
+		if (keyBinds.castspell5.isPressed())
 			castSpell(5);
-		if (KeyBinds.castspell6.isPressed())
+		if (keyBinds.castspell6.isPressed())
 			castSpell(6);
-		if (KeyBinds.castspell7.isPressed())
+		if (keyBinds.castspell7.isPressed())
 			castSpell(7);
-		if (KeyBinds.castspell8.isPressed())
+		if (keyBinds.castspell8.isPressed())
 			castSpell(8);
-		if (KeyBinds.castspell9.isPressed())
-			castSpell(9);
-		if (KeyBinds.castspell0.isPressed())
-			castSpell(0);
-		if (KeyBinds.consider.isPressed())
+		if (keyBinds.consider.isPressed())
 			consider();
-		if (KeyBinds.targetpet.isPressed())
+		if (keyBinds.targetpet.isPressed())
 			targetPet();
 	}
 
 	private static void toggleSitStand() {
 		Minecraft.getInstance().player.sendChatMessage("/sit");
+		LOGGER.info("Sitting");
 	}
 
 	private static void targetPet() {
 		Minecraft.getInstance().player.sendChatMessage("/target pet");
+		LOGGER.info("Targetting pet");
 	}
 
 	private static void consider() {
 		Minecraft.getInstance().player.sendChatMessage("/consider");
+		LOGGER.info("Considering");
 	}
 
 	private static void castSpell(int i) {
 		Minecraft.getInstance().player.sendChatMessage("/castslot " + i);
+		LOGGER.info("Casting spell slot " + i);
 	}
 
 	private static void targetTeamMember(int i) {
 		Minecraft.getInstance().player.sendChatMessage("/target " + i);
-		
+		LOGGER.info("Targetting team member " + i);
 	}
 
 	private static void targetSelf() {
 		Minecraft.getInstance().player.sendChatMessage("/target self");
+		LOGGER.info("Chat message sent");
 	}
 
 	private static void cancelTarget() {
 		Minecraft.getInstance().player.sendChatMessage("/target clear");
+		LOGGER.info("Clearing target");
 	}
 
 	private static void toggleAutoAttack() {
-		Minecraft.getInstance().player.sendChatMessage("/autoattack");
+		LOGGER.info("Auto attacking");
 	}
 
 	private static void targetNearestNpc() {
-		Minecraft.getInstance().player.sendChatMessage("/target nearestnpc");
+		LOGGER.info("Targetting nearest npc");
 	}
 }
