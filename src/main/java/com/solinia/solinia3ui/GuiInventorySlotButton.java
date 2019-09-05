ebe845961c951aafe21cbc95af2638430ff2f286
Michael Bolland <233287+mixxit@users.noreply.github.com>
2019-09-05 13:11:26 +0100

    more backend work for inventory slots

---
+++ src/main/java/com/solinia/solinia3ui/GuiInventorySlotButton.java
@@ -0,0 +1,125 @@
+package com.solinia.solinia3ui;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.lwjgl.opengl.GL11;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.util.ResourceLocation;
+
+public class GuiInventorySlotButton extends Button {
+
+	public GuiInventorySlotButton(int widthIn, int heightIn, int x, int y, String text, Button.IPressable onPress) {
+		super(widthIn, heightIn, x, y, text, onPress);
+	}
+
+	protected static final ResourceLocation GUI_SLOTS_TEXTURES = new ResourceLocation("solinia3ui", "textures/gui/invslots.png");
+	private int slotIcon;
+	
+	@Override
+	public void render(int mouseX, int mouseY, float partialTicks) {
+		if (!this.visible)
+			return;
+		
+		if (getMessage().equals(""))
+			return;
+		
+		String displayIconStr = this.getMessage();
+		try
+		{
+			this.slotIcon = Integer.parseInt(displayIconStr);
+		} catch (Exception e)
+		{
+			return;
+		}
+		
+		int j = 14737632;
+		if (packedFGColor != 0) {
+			j = packedFGColor;
+		} else if (!this.active) {
+			j = 10526880;
+		} else if (this.isHovered()) {
+			j = 16777120;
+		}
+		
+		GlStateManager.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+		
+		if (this.slotIcon > -1)
+		{
+			Minecraft.getInstance().getTextureManager().bindTexture(GUI_SLOTS_TEXTURES);
+			Point position = getSlotIconPosition(this.slotIcon);
+			this.blit(this.x, this.y, position.y,position.x, this.width, this.width-1);
+		}
+		
+		this.renderBg(Minecraft.getInstance(), mouseX, mouseY);
+		String displayString = getSlotNameById(this.slotIcon);
+		this.drawStringCenteredScale(Minecraft.getInstance().fontRenderer, displayString, this.x + this.width /2,this.y + (this.height - 8) + Minecraft.getInstance().fontRenderer.FONT_HEIGHT, 0.5f, j);
+
+	}
+	
+	private Point getSlotIconPosition(int slotIconId) {
+		int slotSize = 20;
+		switch(slotIconId)
+		{
+			case 0:
+				return new Point(2*slotSize,1*slotSize); // FINGER 3x2
+			case 1:
+				return new Point(1*slotSize,0*slotSize); // SHOULDER 2x1
+			case 2:
+				return new Point(0*slotSize,1*slotSize); // NECK 1x2
+			case 3:
+				return new Point(0*slotSize,0*slotSize); // EAR 1x1
+			case 4:
+				return new Point(1*slotSize,3*slotSize); // FOREARMS 2x4
+			case 5:
+				return new Point(1*slotSize,1*slotSize); // ARMS 2x2
+			case 6:
+				return new Point(2*slotSize,0*slotSize); // HANDS 3x1
+			case 7:
+				return new Point(1*slotSize,4*slotSize); // WAIST 2x5
+			default:
+				return new Point(2*slotSize,1*slotSize); // FINGER 3x2
+		}
+	}
+
+
+	public String getSlotNameById(int slotId)
+	{
+		switch(slotId)
+		{
+		case 0:
+			return "Fingers";
+		case 1:
+			return "Shoulders";
+		case 2:
+			return "Neck";
+		case 3:
+			return "Ears";
+		case 4:
+			return "Forearms";
+		case 5:
+			return "Arms";
+		case 6:
+			return "Hands";
+		case 7:
+			return "Waist";
+			default:
+				return "";
+				
+		}
+	}
+	
+	public void drawStringCenteredScale(FontRenderer fontRendererIn, String text, int x, int y, float size, int color) {
+        GL11.glScalef(size,size,size);
+        float mSize = (float)Math.pow(size,-1);
+        this.drawCenteredString(fontRendererIn,text,Math.round(x / size),Math.round(y / size),color);
+        GL11.glScalef(mSize,mSize,mSize);
+    }
+}
