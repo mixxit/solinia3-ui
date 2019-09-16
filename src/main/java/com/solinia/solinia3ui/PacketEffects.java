7491630a9360339bb427c878bbf400fedb25ce7b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-16 21:53:51 +0000

    Deploy mixxit/solinia3-ui to github.com/mixxit/solinia3-ui.git:gh-pages

---
+++ src/main/java/com/solinia/solinia3ui/PacketEffects.java
@@ -0,0 +1,95 @@
+package com.solinia.solinia3ui;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import io.netty.buffer.ByteBufInputStream;
+import net.minecraft.network.PacketBuffer;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+public class PacketEffects {
+	Effects effects = new Effects();
+	
+	PacketEffects(PacketBuffer buf) throws RuntimeException
+    {
+    	try(DataInputStream stream = new DataInputStream(new ByteBufInputStream(buf)))
+    	{
+        	fromPacketData(stream.readUTF());
+    	} catch (IOException e) {
+    		throw new RuntimeException("impossible", e);
+		} catch (InvalidPacketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
+	
+	public Effects getEffects()
+	{
+		return this.effects;
+	}
+	
+	public void fromPacketData(String data) throws InvalidPacketException
+	{
+		if (data == null)
+			throw new InvalidPacketException("Packet data is empty");
+
+		// now pages
+		this.effects = new Effects();
+		
+		if (data.equals(""))
+			return;
+		
+		String[] dataArray = data.split("\\^",-1);
+		
+		for(int i = 0; i < dataArray.length; i++)
+		{
+			String[] effectArray = dataArray[i].split("\\|",-1);
+			int SpellId = Integer.parseInt(effectArray[0]);
+			int Icon = Integer.parseInt(effectArray[1]);
+			int NewIcon = Integer.parseInt(effectArray[2]);
+			int MemIcon = Integer.parseInt(effectArray[3]);
+			String Name = effectArray[4];
+			this.effects.effectSlots.put(SpellId, new EffectSlot(SpellId,Icon,MemIcon,NewIcon,Name));
+		}
+	}
+	
+	public String toPacketData()
+	{
+		String packetData = "";
+		boolean first = true;
+		for(Map.Entry<Integer, EffectSlot> entry : this.effects.effectSlots.entrySet())
+		{
+			if (first)
+				first = false;
+			else
+				packetData += "^";
+			
+			packetData += 
+					+ entry.getValue().SpellId + "|" 
+					+ entry.getValue().Icon + "|" 
+					+ entry.getValue().NewIcon + "|" 
+					+ entry.getValue().MemIcon + "|" 
+					+ entry.getValue().Name;
+		}
+		
+		return packetData;
+	}
+	
+	public void fromData(Effects effects) {
+		this.effects = effects;
+	}
+	
+	public void encode(PacketBuffer buf)
+    {
+    	buf.writeString(toPacketData());
+    }
+	
+	public void handle(Supplier<NetworkEvent.Context> context)
+	{
+		context.get().enqueueWork(() -> ClientState.getInstance().setEffects(this.effects));
+    	context.get().setPacketHandled(true);
+	}
+}
--- src/main/java/com/solinia/solinia3ui/RenderInventoryHandler.java
+++ src/main/java/com/solinia/solinia3ui/RenderInventoryHandler.java
@@ -14,6 +14,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
+import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent.Post;
 import net.minecraftforge.client.event.GuiScreenEvent.MouseClickedEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -39,25 +40,12 @@ public class RenderInventoryHandler {
 		if (!(event.getGui() instanceof InventoryScreen))
 			return;
 
-		event.addWidget(new Button(Minecraft.getInstance().mainWindow.getScaledWidth()-width,20,width,width,"Spell",new GuiAddItemToSpellbookButtonPressable()));
-		event.addWidget(new Button(Minecraft.getInstance().mainWindow.getScaledWidth()-width,40,width,width,"Reagent",new GuiAddItemToReagentsButtonPressable()));
-		// Inventory slots
-		// fingers
-		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,20,width,width,"0",new GuiInventorySlotButtonPressable(0)));
-		//shoulders
-		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,45,width,width,"1",new GuiInventorySlotButtonPressable(1)));
-		//neckl
-		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,70,width,width,"2",new GuiInventorySlotButtonPressable(2)));
-		//ears
-		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,95,width,width,"3",new GuiInventorySlotButtonPressable(3)));
-		//forearms
-		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,120,width,width,"4",new GuiInventorySlotButtonPressable(4)));
-		//arms
-		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,145,width,width,"5",new GuiInventorySlotButtonPressable(5)));
-		//hands
-		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,170,width,width,"6",new GuiInventorySlotButtonPressable(6)));
-		//waist
-		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,195,width,width,"7",new GuiInventorySlotButtonPressable(7)));
+		addSlots(event);
+		addSlotCloseButtons(event);
+		
+	}
+
+	private void addSlotCloseButtons(Post event) {
 
 		// close buttons
 		event.addWidget(new Button(Minecraft.getInstance().mainWindow.getScaledWidth()-width,20,width,width,"Spell",new GuiAddItemToSpellbookButtonPressable()));
@@ -79,7 +67,28 @@ public class RenderInventoryHandler {
 		event.addWidget(new Button(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3+20,170,5,5,"x",new GuiInventorySlotUnequipButtonPressable(6)));
 		//waist
 		event.addWidget(new Button(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3+20,195,5,5,"x",new GuiInventorySlotUnequipButtonPressable(7)));
+	}
 
+	private void addSlots(Post event) {
+		event.addWidget(new Button(Minecraft.getInstance().mainWindow.getScaledWidth()-width,20,width,width,"Spell",new GuiAddItemToSpellbookButtonPressable()));
+		event.addWidget(new Button(Minecraft.getInstance().mainWindow.getScaledWidth()-width,40,width,width,"Reagent",new GuiAddItemToReagentsButtonPressable()));
+		// Inventory slots
+		// fingers
+		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,20,width,width,"0",new GuiInventorySlotButtonPressable(0)));
+		//shoulders
+		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,45,width,width,"1",new GuiInventorySlotButtonPressable(1)));
+		//neckl
+		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,70,width,width,"2",new GuiInventorySlotButtonPressable(2)));
+		//ears
+		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,95,width,width,"3",new GuiInventorySlotButtonPressable(3)));
+		//forearms
+		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,120,width,width,"4",new GuiInventorySlotButtonPressable(4)));
+		//arms
+		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,145,width,width,"5",new GuiInventorySlotButtonPressable(5)));
+		//hands
+		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,170,width,width,"6",new GuiInventorySlotButtonPressable(6)));
+		//waist
+		event.addWidget(new GuiInventorySlotButton(Minecraft.getInstance().mainWindow.getScaledWidth()-width*3,195,width,width,"7",new GuiInventorySlotButtonPressable(7)));
 		
 	}
 }
